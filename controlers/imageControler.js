b8161ce755274f2b64ce008d79942b6734806301
StefanIliev0 <106112761+StefanIliev0@users.noreply.github.com>
2023-08-08 21:46:53 +0300

    update

--- controlers/imageControler.js
+++ controlers/imageControler.js
@@ -5,22 +5,22 @@ const imageService = require(`../services/imagrServise`) ;
 const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
-    accessKeyId: 'YOUR_ACCESS_KEY_ID',
-    secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+    accessKeyId: 'ASIAURG6Q27LY5UYMWNT',
+    secretAccessKey: 'MNsbJodurhH9xLo09zpb4Ax0eleqVAy8DUG8LvFJ',
   });
-  const upload = multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: 'YOUR_BUCKET_NAME',
-      acl: 'public-read',  // Позволява публичен достъп до файловете
-      metadata: function (req, file, cb) {
-        cb(null, { fieldName: file.fieldname });
-      },
-      key: function (req, file, cb) {
-        cb(null, Date.now().toString() + '-' + file.originalname);
-      },
-    }),
-  });
+//   const upload = multer({
+//     storage: multerS3({
+//       s3: s3,
+//       bucket: 'cyclic-kind-lime-attire-eu-central-1',
+//       acl: 'public-read',  // Позволява публичен достъп до файловете
+//       metadata: function (req, file, cb) {
+//         cb(null, { fieldName: file.fieldname });
+//       },
+//       key: function (req, file, cb) {
+//         cb(null, Date.now().toString() + '-' + file.originalname);
+//       },
+//     }),
+//   });
 
 // app.put('*', async (req,res) => {
 //     let filename = req.path.slice(1)
@@ -35,39 +35,39 @@ const s3 = new AWS.S3({
   
 //     res.set('Content-type', 'text/plain')
 //     res.send('ok').end()
-router.post('/upload', upload.single('image'), (req, res) => {
-    res.json({ imageUrl: req.file.location });
-  });
+// router.post('/upload', upload.single('image'), (req, res) => {
+//     res.json({ imageUrl: req.file.location });
+//   });
 
-// router.post(`/:fileName`  , async(req ,res) => {
-//     const textMessageId  = req.params.fileName ; 
+router.post(`/:fileName`  , async(req ,res) => {
+    const fileName  = req.params.fileName ; 
+    try{
+        let s3File = await s3.putObject({
+      Body: JSON.stringify(req.body),
+      Bucket: 'cyclic-kind-lime-attire-eu-central-1',
+      Key: fileName,
+    }).promise();
+    res.set('Content-type', s3File.ContentType)
+    res.json({imageUrl :`{URL}/${fileName}`})
+}catch(err){
+    res.status(400).json({ error : err.message });
+}} );
+// router.get(`/:filename`  , async(req ,res) => {
+//     const {username , password} = req.body ; 
 //     try{
-//         let s3File = await s3.putObject({
-//       Body: JSON.stringify(req.body),
-//       Bucket: process.env.BUCKET,
-//       Key: filename,
-//     }).promise();
-//     res.set('Content-type', s3File.ContentType)
-//     res.json({imageUrl :`{URL}/${fileName}`})
-// }catch(err){
-//     res.status(400).json({ error : err.message });
-// }} );
-router.get(`/:filename`  , async(req ,res) => {
-    const {username , password} = req.body ; 
-    try{
-    const user =  await authServices.addNewUser(username , password) ; 
-    res.json(user);
-    }catch(err){
-        res.status(400).json({ error : err.message });
-    }
-} );
-router.delete(`/`  , async(req ,res) => {
-    const {username , password} = req.body ; 
-    try{
-    const user =  await authServices.addNewUser(username , password) ; 
-    res.json(user);
-    }catch(err){
-        res.status(400).json({ error : err.message });
-    }
-} );
+//     const user =  await authServices.addNewUser(username , password) ; 
+//     res.json(user);
+//     }catch(err){
+//         res.status(400).json({ error : err.message });
+//     }
+// } );
+// router.delete(`/`  , async(req ,res) => {
+//     const {username , password} = req.body ; 
+//     try{
+//     const user =  await authServices.addNewUser(username , password) ; 
+//     res.json(user);
+//     }catch(err){
+//         res.status(400).json({ error : err.message });
+//     }
+// } );
 module.exports = router ;
