de5efe38443914ea74cd768e2470102c5502bf63
StefanIliev0 <106112761+StefanIliev0@users.noreply.github.com>
2023-08-11 20:54:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended : false})) ;
 app.use(express.json())
+app.use(express.raw({ type: 'image/*', limit: '5mb' }))
 app.use(authMiddkewares.authentication);
 
 app.use(router)
